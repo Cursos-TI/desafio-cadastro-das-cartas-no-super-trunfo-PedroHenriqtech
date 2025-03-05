bbbdca28bea6c8c9c40634f1c0446773e098dba0
PedroHenriqtech <pedrohenriquejeronimo.fs@gmail.com>
2025-03-05 17:03:11 +0000

    modifiquei a mensagem do printf

--- Super_trunfo.c
+++ Super_trunfo.c
@@ -4,11 +4,11 @@
 // Tema 1 - Cadastro das Cartas
 
 int main (){
-char carta[50];
+
 char cidade1[50];
 char cidade2[50];
-char estado1[50];
-char estado2[50];
+char estado1[2];
+char estado2[2];
 char codigo1[50];
 char codigo2[50];
 int populacao1;
@@ -25,16 +25,17 @@ int pontosTuristicos2;
 
 //Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
 
-printf("Super Trunfo - Países \n");
+printf("--* Super Trunfo - Países *-- \n\n");
+
 printf("Carta 1: \n");
 
-printf("Digite o estado (ex: Ceará): "); //Digite conforme o exemplo dentro do parênteses
+printf("Digite a letra inicial do estado (ex: A): "); //Digite conforme o exemplo dentro do parênteses
 scanf("%s", &estado1); //Utilize a função scanf para capturar as entradas do usuário para cada atributo.
 
-printf("Digite o código (ex: A01): "); //Digite conforme o exemplo dentro do parênteses
+printf("Digite o código ( A letra do estado seguida de um número - ex: A01): "); //Digite conforme o exemplo dentro do parênteses
 scanf("%s", &codigo1); //Utilize a função scanf para capturar as entradas do usuário para cada atributo.
 
-printf("Digite o nome da cidade (ex: Fortaleza): "); //Digite conforme o exemplo dentro do parênteses
+printf("Digite o nome da cidade (ex: Manaus): "); //Digite conforme o exemplo dentro do parênteses
 scanf("%s", &cidade1); //Utilize a função scanf para capturar as entradas do usuário para cada atributo.
 
 printf("Digite a população (ex:50000) cinquenta mil: "); //Digite conforme o exemplo dentro do parênteses
@@ -43,7 +44,7 @@ scanf("%d", &populacao1); //Utilize a função scanf para capturar as entradas d
 printf("Digite a área (ex: 1521.11) km²: "); //Digite conforme o exemplo dentro do parênteses
 scanf("%f", &area1); //Utilize a função scanf para capturar as entradas do usuário para cada atributo.
 
-printf("Digite o Pib (ex: 700.28) milhões: "); //Digite conforme o exemplo dentro do parênteses
+printf("Digite o Pib (ex: 2000000000.00) duzentos bilhões: "); //Digite conforme o exemplo dentro do parênteses
 scanf("%f", &pib1); //Utilize a função scanf para capturar as entradas do usuário para cada atributo.
 
 printf("Digite os números de pontos turísticos (ex: 20): "); //Digite conforme o exemplo dentro do parênteses
@@ -54,28 +55,34 @@ scanf("%d", &pontosTuristicos1); //Utilize a função scanf para capturar as ent
 
 printf("\nCarta 2: \n");
 
-printf("Digite o estado (ex: Pernambuco): "); //Digite conforme o exemplo dentro do parênteses
+printf("Digite a letra inicial do estado (ex: C): "); //Digite conforme o exemplo dentro do parênteses
 scanf("%s", &estado2); //Utilize a função scanf para capturar as entradas do usuário para cada atributo.
 
-printf("Digite o código (ex: B01): "); //Digite conforme o exemplo dentro do parênteses
+printf("Digite o código ( A letra do estado seguida de um número - ex: C01): "); //Digite conforme o exemplo dentro do parênteses
 scanf("%s", &codigo2); //Utilize a função scanf para capturar as entradas do usuário para cada atributo.
 
-printf("Digite o nome da cidade (ex: Recife): "); //Digite conforme o exemplo dentro do parênteses
+printf("Digite o nome da cidade (ex: Fortaleza): "); //Digite conforme o exemplo dentro do parênteses
 scanf("%s", &cidade2); //Utilize a função scanf para capturar as entradas do usuário para cada atributo.
 
-printf("Digite a população (ex: 200000) duzentos mil: "); //Digite conforme o exemplo dentro do parênteses
+printf("Digite a população (ex: 400000) quatrocentos mil: "); //Digite conforme o exemplo dentro do parênteses, ou seja, somente números
 scanf("%d", &populacao2); //Utilize a função scanf para capturar as entradas do usuário para cada atributo.
 
-printf("Digite a área (ex: 5500.00 km²): "); //Digite conforme o exemplo dentro do parênteses
+printf("Digite a área (ex: 5500.00) km²: "); //Digite conforme o exemplo dentro do parênteses
 scanf("%f", &area2); //Utilize a função scanf para capturar as entradas do usuário para cada atributo.
 
-printf("Digite o Pib (ex: 800.00) milhões: "); //Digite conforme o exemplo dentro do parênteses
+printf("Digite o Pib (ex: 2000000000.00) duzentos bilhões: "); //Digite conforme o exemplo dentro do parênteses
 scanf("%f", &pib2); //Utilize a função scanf para capturar as entradas do usuário para cada atributo.
 
 printf("Digite os números de pontos turísticos (ex: 15): "); //Digite conforme o exemplo dentro do parênteses
 scanf("%d", &pontosTuristicos2); //Utilize a função scanf para capturar as entradas do usuário para cada atributo.
 
-//Imprimindo as cartas 1 e 2
+double pibPercapita1 = pib1 / populacao1;
+double pibPercapita2 = pib2 / populacao2;  
+float densidadePopulacional1 = populacao1 / area1;
+float densidadePopulacional2 = populacao2 / area2;
+
+
+//Imprimindo carta 1 
 // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
 
 printf("\nSuper trunfo - Carta 1: \n");
@@ -84,8 +91,13 @@ printf("Código: %s\n", codigo1);
 printf("Cidade: %s\n", cidade1);
 printf("População: %d habitantes\n", populacao1);
 printf("Área: %.2f km²\n", area1);
-printf("Pib: %.2f milhões de reais\n", pib1);
+printf("Pib: %.2f bilhões de reais\n", pib1);
 printf("Pontos turísticos: %d\n", pontosTuristicos1);
+printf("PIB per Capita: %.2f reais\n", pibPercapita1);
+printf("Densidade populacional: %.2f hab/km²\n" , densidadePopulacional1);
+
+//imprimindo carta 2
+// Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
 
 printf("\nSuper trunfo - Carta 2: \n");
 printf("Estado: %s\n", estado2);
@@ -93,8 +105,10 @@ printf("Código: %s\n", codigo2);
 printf("Cidade: %s\n", cidade2);
 printf("População: %d habitantes\n", populacao2);
 printf("Área: %.2f km²\n", area2);
-printf("Pib: %.2f milhões de reais\n", pib2);
+printf("Pib: %.2f bilhões de reais\n", pib2);
 printf("Pontos turísticos: %d\n", pontosTuristicos2);
+printf("PIB per Capita: %.2f reais\n", pibPercapita2);
+printf("Densidade populacional: %.2f hab/km²\n" , densidadePopulacional2);
 
 return 0;
 }
